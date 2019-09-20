d074362ccaf24f53a1c1c8a2a0a117ae4f80b6f1
Ludvik Fjeld <hdguys11@gmail.com>
2019-09-20 11:56:07 +0200

    Update Python4Physics.py
    updates, fixes.

--- Python4Physics.py
+++ Python4Physics.py
@@ -6,10 +6,36 @@ Created on Thu Sep 19 10:44:08 2019
 """
 
 c,cv,h,B = 3e8, 0.751*3e8, 6.63e-34, 2.18e-18
-def e(n1,n2):
+
+"""
+Calculates energy needed to exitate or deexitate the current electron shell,
+where n1 is the starting shell, and n2 is the ending shell.
+"""
+def ehyd(n1,n2):
+    """
+    Joule
+    """
     return (-B/n1**2)-(-B/n2**2)
-def hz(λ):
-    return c/λ
+
+"""
+Calculates Frequency by dividing speed of the wave(c or other if that is specified) by wavelength(λ)
+"""
+def hz(λ, speed=c):
+    """
+    Hertz
+    """
+    return speed/λ
+
+"""
+Calculates Energy for a given foton. 
+Takes Plancks-constant(h) and frequency(f)
+"""
+def e(f):
+    """
+    Joule
+    """
+    return h*f
+
 print('This is a modified Python Console made for Physics 1')
 print('The following variables have been defined:')
 print(f'c : {c}\ncv : {cv}\nh : {h}\nB : {B}')
