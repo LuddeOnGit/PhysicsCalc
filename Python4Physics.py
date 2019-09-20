fe4bc64a1d6397ed97b3a8823ef90a139d32aa1e
Victor Apeland <victor.apeland@icloud.com>
2019-09-20 11:59:15 +0200

    Added SI prefixes

--- Python4Physics.py
+++ Python4Physics.py
@@ -5,6 +5,10 @@ Created on Thu Sep 19 10:44:08 2019
 @author: Ludvik Fjeld
 """
 
+# SI prefixes
+a,f,p,n,µ,m,c,d,da,he,k,M,G,T,P,E = 10e-18,10e-15,10e-12,10e-9,10e-6,10e-3,10e-2,10e-1,10e1,10e2,10e3,10e6,10e9,10e12,10e15,10e18
+
+# Physical constants
 c,cv,h,B = 3e8, 0.751*3e8, 6.63e-34, 2.18e-18
 
 """
