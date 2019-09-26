aed2b351ea969824e3c568adbf305402db3aca76
Jakobgg1243 <Jakob.gregusson@gmail.com>
2019-09-26 17:56:24 +0200

    Added electron configuration

--- Python4Physics.py
+++ Python4Physics.py
@@ -68,7 +68,7 @@ Gives energy amount by using Einstein's formula given mass and lightspeed square
 def mass(m, v=c): return m*(v**2)
 
 """
-converts u to kg, m = mass in u
+Converts u to kg, m = mass in u
 """
 def massConvU(m): return m*u
 
@@ -77,86 +77,20 @@ Converts kg to kg, m = mass in kg
 """
 def massConvKg(m): return m/u
 
-
 """
-Returns in the form of a string the electon configuration of the various orbitals.
-While technically lernt in chemistry, this is relevant in all the natural sciences.
-Takes electron count (ec), and whether to shorten the result with the last noble gas (short)
-
-⁰¹²³⁴⁵⁶⁷⁸⁹
-Some examples to help me think
- 1: 1s¹
- 2: 1s²
- 3: 1s²2s¹
- 4: 1s²2s²
- 5: 1s²2s²2p¹
- 6: 1s²2s²2p²
- 7: 1s²2s²2p³
- 8: 1s²2s²2p⁴
- 9: 1s²2s²2p⁵
-10: 1s²2s²2p⁶
-11: 1s²2s²2p⁶3s¹
-12: 1s²2s²2p⁶3s²
-13: 1s²2s²2p⁶3s²3p¹
-14: 1s²2s²2p⁶3s²3p²
-15: 1s²2s²2p⁶3s²3p³
-16: 1s²2s²2p⁶3s²3p⁴
-17: 1s²2s²2p⁶3s²3p⁵
-18: 1s²2s²2p⁶3s²3p⁶
-19: 1s²2s²2p⁶3s²3p⁶4s¹
-20: 1s²2s²2p⁶3s²3p⁶4s²
-21: 1s²2s²2p⁶3s²3p⁶4s²3d¹
-...
-30: 1s²2s²2p⁶3s²3p⁶4s²3d¹⁰
-31: 1s²2s²2p⁶3s²3p⁶4s²3d¹⁰4p¹
-36: 1s²2s²2p⁶3s²3p⁶4s²3d¹⁰4p⁶
-37: 1s²2s²2p⁶3s²3p⁶4s²3d¹⁰4p⁶5s¹
+Calculates electron configuration for a number of electrons (ec)
 """
-def electronConfig(ec, short=False):
     
-    orbitalNames  = ["s","p","d","f"]
+def electronConfiguration(ec): 
+    print(find_next_shell(ec))
     
-    
-    # Hard coding in the first electron to give the loop a nice start
-    shells = [(0,0,1)]
-    
-    # Iterating over the rest to add them
-    for i in range(ec - 1):
-        if shells[-1][2] != (4 * shells[-1][1] + 2): # Last subshell isn't full
-            # Add one electron to the last shell. Weird because Python doesn't like editing tuple items
-            shells[-1] = (shells[-1][0], shells[-1][1], shells[-1][2] + 1)
-            # Basically shells[-1][2] += 1
-        else:
-            # Create new shell
-            
-            lastShellEnergy = shells[-1][0] + shells[-1][1]
-            
-            # The toBeat variable will be changed on every iteration of the inner loop if the shell-subshell sum is lower
-            # Initialized with an arbitrary large value that will be beaten on the first iteration. 
-            # If (100,100) is seen outside of these two lines, something is very wrong.
-            toBeat = (100, 100)
-            # Loop over the first to last possible shell
-            for m in range(1, lastShellEnergy + 2):
-                # Loop over subshells
-                for s in range(m + 1):
-                    # If the subshell we're checking is lower energy than toBeat, replace toBeat.
-                    if (m + s) < (toBeat[0] + toBeat[1]) or (((m + s) == (toBeat[0] + toBeat[1])) and m < toBeat[0]):
-                        # If the supposedly lower energy shell is actually different from the last
-                        for (om, os, _) in shells: 
-                            if (om == m and os == s): 
-                                print(m,s)
-                                continue
-                        toBeat = (m,s)
-                         
-                    
-            print()        
-            
-            shells.append((toBeat[0],toBeat[1],1))    
-            
-    
-    # Turn shells into a string
-
-    return shells
+def find_next_shell(ec, index = 0):
+    shells = ["1s", "2s", "2p", "3s", "3p", "4s", "3d", "4p", "5s", "4d", "5p", "6s", "4f"] 
+    electrons_in_shells = [2, 2, 6, 2, 6, 2, 10, 6, 2, 10, 6, 2, 14]
+    if ec - electrons_in_shells[index] > 0:          
+        return shells[index] + str(electrons_in_shells[index]) + ", " + find_next_shell(ec-electrons_in_shells[index], index + 1)
+    else:
+        return shells[index] + str(ec)
 
 print('This is a modified Python Console made for Physics 1')
 print('The following constants have been defined:')
