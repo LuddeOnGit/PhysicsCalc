f96a181e2c0db52c19674089ea18038b797ec0ac
Jakobgg1243 <Jakob.gregusson@gmail.com>
2019-09-28 16:52:24 +0200

    Started using tkinter to make GUI

--- GUI.py
+++ GUI.py
@@ -1,33 +1,38 @@
-import PySimpleGUI as sg
+from tkinter import * 
 from Python4Physics import *
 
-"""
-GUI
-"""
+def main():
+    def u_to_kg():
+        if len(uInput.get()) > 0:
+            kgOutput["text"] = str(massConvU(float(uInput.get()))) + "kg"
+    def kg_to_u():
+        if len(kgInput.get()) > 0:
+            uOutput["text"] = str(massConvKg(float(kgInput.get()))) + "u"
+        
+    window = Tk()
+    window.title("Physics Stuff")
+    #window.configure(background = "")
 
-def event_loop():
-    while True:
-        event, values = window.read()    
-        if event in (None, "Cancel"):
-            break
-        if values["-uInput-"] != " ":
-            window["-kgOutput-"].Update(massConvU(float(values["-uInput-"])))
-        if values["-kgInput-"] != " ":
-            window["-uOutput-"].Update(massConvKg(float(values["-kgInput-"])))
-        if values["-fotonEnergyInput-"] != " ":
-            window["-wlOutput-"].Update(wlPhoton(float(values["-fotonEnergyInput-"])))
-        if event == "Mass":
-            window["-missingMassOutput-"].Update(massLeft(float(values["-beforeMassInput-"]), float(values["-afterMassInput-"])))
-        if event == "Energy":
-            window["-energyOutput-"].Update(mass(massLeft(float(values["-beforeMassInput-"]), float(values["-afterMassInput-"]))))
+    """
+    u to kg
+    """
+    Label(window, text="Convert u to kg: ", font="none 12").grid(row=0, column=0, sticky=W)
+    uInput = Entry(window, width=8)
+    uInput.grid(row=0, column=1, sticky=W)
+    kgOutput = Label(window, text="0kg", font="none 12")
+    kgOutput.grid(row=0, column=2, sticky=W)
+    Button(window, text="SUBMIT", width=6, command=u_to_kg).grid(row=0, column=3, sticky=W)
 
-    window.Close()
+    """'
+    kg to u
+    """
+    Label(window, text="Convert kg to u: ", font="none 12").grid(row=1, column=0, sticky=W)
+    kgInput = Entry(window, width=8)
+    kgInput.grid(row=1, column=1, sticky=W)
+    uOutput = Label(window, text="0u", font="none 12")
+    uOutput.grid(row=1, column=2, sticky=W)
+    Button(window, text="SUBMIT", width=6, command=kg_to_u).grid(row=1, column=3, sticky=W)
 
-layout = [[sg.Text("Convert u to kg:"), sg.InputText(" ", key="-uInput-", size=(5,1)), sg.Text("", key="-kgOutput-", size=(30,1))],
-          [sg.Text("Convert kg to u:"), sg.InputText(" ", key="-kgInput-", size=(5,1)), sg.Text("", key="-uOutput-", size=(30,1))],
-          [sg.Text("Convert foton energy to wavelength:"), sg.InputText(" ", key="-fotonEnergyInput-", size=(7,1)), sg.Text("", key="-wlOutput-", size=(20,1))],
-          [sg.Text("Calculate missing mass (before, after):"), sg.InputText(" ", key=("-beforeMassInput-"), size=(7,1)), sg.InputText(" ", key=("-afterMassInput-"), size=(7,1)), sg.Text("", key=("-massOutput-"), size=(8,1)),
-           sg.Text("", key=("-missingEnergyOutput-"), size=(15,1)), sg.Button("Energy"), sg.Button("Mass")],
-          [sg.Button("Ok"), sg.Button("Cancel")]]
+    window.mainloop()
 
-window = sg.Window("Physics stuff", layout)
+
