d9761a947b9c616f7084a29cb5af47e244726712
Victor Apeland <victor.apeland@icloud.com>
2019-09-30 11:13:05 +0200

    Made elements regular size and moved element GUI to PeriodicTable

--- Elements.py
+++ Elements.py
@@ -81,11 +81,7 @@ class Element:
                 return shells[index] + str(ec)
         return find_next_shell(self.number)
 
-    def elementInfo(self):
-        infoWindow = Tk()
-        infotxt = str(self.info())
-        infoLabel = Label(infoWindow, text=infotxt, font="none 16")
-        infoLabel.grid(row=8, column=0, pady = 10, sticky=E)
+    
         
 elements = [ # Number  | Symbol      | Element name         | Boiling point 1 atm (°K)| Melting point in 1 atm (°K)| mass (u)
     Element(number=   0, symbol= "E" , name= "Errorium"     , boilingPoint= "Error"   , meltingPoint= "Error"      , mass= "Error"  ), # So the index is the same as the atomic number
--- PeriodicTable.py
+++ PeriodicTable.py
@@ -7,19 +7,24 @@
 from tkinter import *
 import Elements
 
-
 def PeriodicTable():
     window = Tk()
     window.title = "Periodic Table"
+
+    def elementInfo(element):
+        infoWindow = Tk()
+        infotxt = str(element.info())
+        infoLabel = Label(infoWindow, text=infotxt, font="none 16")
+        infoLabel.grid(row=8, column=0, pady = 10, sticky=E)
     
     """
-    Added to give the user an interactive periodic table, ment to click on an element and get pop-up with information
+    Added to give the user an interactive periodic table, meant to click on an element and get pop-up with information
     """
         
     for element in Elements.elements:
         if type(element.group()) == int:
             #Lambda is used to prevent the function from running on startup so that you can add parameters to function call
-            button = Button(window, text=element.symbol, font="none 16", command= element.elementInfo)
+            button = Button(window, text=element.symbol, font="none 16", command= lambda: elementInfo(element), height = 2, width = 3)
             button.grid(row=element.period(), column=element.group(), pady=2, sticky=W)
     
     window.mainloop()
