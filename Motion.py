f256224f742cac51b4e13966250383506c0c8bd8
Victor Apeland <victor.apeland@icloud.com>
2019-10-30 09:59:48 +0100

    Tidied up Motion-py a bit

--- Motion.py
+++ Motion.py
@@ -6,32 +6,25 @@ Created on Tue Oct 29 15:16:29 2019
 @author: victorapeland
 """
 
+# Usage: use the factor you want as the function name and type in what you already have as parameters. Don't enter the ones you don't have
 
 def endVelocity(startV="", a="", dt="", ds=""):
     if "" not in [startV,a,dt] : return startV + a * dt # Formula 1
     if "" not in [dt,startV,ds]: return startV - 2*ds/dt
     if "" not in [a,ds,startV]  : return (2*a*ds - startV**2)**(1/2)
-    return "no"
+    return "Insufficient data entered."
     
-
 def time(startV="", endV="",  a="", ds=""):
     if "" not in [startV,endV, a]: return (endV - startV)/a
     if "" not in [endV,startV,ds]: return ds/((startV + endV)/2)
     if "" not in [startV,a,ds]: 
         sqrt = startV**2 + 2*a(ds)
-        if sqrt < 0: return
-        value1 = (startV + sqrt**(1/2))/a
-        value2 = (startV - sqrt**(1/2))/a # TODO: find some way of determinint which value is relevant
-        return value1
+        if sqrt >= 0:
+            value1 = (startV + sqrt**(1/2))/a
+            value2 = (startV - sqrt**(1/2))/a # TODO: find some way of determine which value is relevant
+            return value1
+    return "Insufficient data entered."
     
-
-        
-def beginning_velocity(ds="", a="", endV="", dt=""):
-    if "" not in [a, endV, dt]: return endV - a * dt
-    if "" not in [ds, endV, dt]: return 2 * ds / dt - endV
-    if "" not in [ds, endV, a]: return (endV * 2 - 2 * a * ds) * (1/2)
-    return "You noob!!"   
-
 def acceleration(startV="", endV="", dt="", ds=""):
     if "" not in [endV, startV, dt]: return (endV - startV) / dt
     if "" not in [ds, startV, dt]: return 2 * (ds - startV * dt) / dt ** 2
@@ -41,13 +34,13 @@ def distance(startV="", a="", endV="", dt=""):
     if "" not in [startV, endV, dt]: return 1/2 * dt * (startV + endV) 
     if "" not in [a, startV, dt]: return startV * dt + 1/2 * a * dt ** 2
     if "" not in [startV, endV, a]: return (startV * 2 - endV * 2) / (2 * a)
-    return "You noob!!"
+    return "Insufficient data entered."
 
 def startVelocity(ds="", a="", endV="", dt=""):
     if "" not in [a, endV, dt]: return endV - a * dt
     if "" not in [ds, endV, dt]: return 2 * ds / dt - endV
     if "" not in [ds, endV, a]: return (endV * 2 - 2 * a * ds) * (1/2)
-    return "You noob!!"
+    return "Insufficient data entered."
 
 # If you want the straight formulae without any funny business, here you go:
 """
