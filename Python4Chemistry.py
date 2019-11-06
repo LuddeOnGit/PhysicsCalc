5491c150de9b24decbdb3a0e281de5263c7ec6bb
Jakobgg1243 <55840686+Jakobgg1243@users.noreply.github.com>
2019-11-06 11:04:06 +0100

    Added option to have parenthesis in formula

--- Python4Chemistry.py
+++ Python4Chemistry.py
@@ -5,14 +5,19 @@ def find_number_of_particles(grams, atom_number):
 
 def find_grams_from_number_of_particles(particles, atom_number):
     return particles * elements[atom_number].mass
-    
-def massFromFormula(formula):
+
+def massFromFormula(formula, amount = 1):
     if formula == "": return 0
-    
-    print(formula)
+    if formula[0] == "(":
+        l = formula.index(")")
+        content = formula[1:l]
+        amount = int(formula[l+1:])
+        return massFromFormula(content, amount)
     
     i = 1
-    while (i < len(formula) and not formula[i].isupper()): i += 1
+    while (i < len(formula) and not formula[i].isupper()):
+        if formula[i] == "(": break
+        else: i += 1
     
     expression = formula[0:i]
     
@@ -20,15 +25,11 @@ def massFromFormula(formula):
     while (j < len(expression) and not formula[j].isdigit()): j += 1
     
     symbol = expression[:j]
-    count = int(expression[j:] if expression[j:] != "" else 1)
-    
-    print(symbol, count)
+    count = int(expression[j:] if expression[j:] != "" else 1) * amount
     
     mass = elements[indexBySymbol(symbol)].mass
     
-    return count * mass + massFromFormula(formula[i:])
-    
-    
+    return float(count) * mass + massFromFormula(formula[i:], amount)
     
     
     
