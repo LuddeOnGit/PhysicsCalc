7078452e47a94b60605ce80f2c9e54cc34bc26ec
Ludvik Fjeld <hdguys11@gmail.com>
2020-02-11 08:29:27 +0100

    Create Electricity.py

---
+++ Electricity.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 11:41:27 2020
+
+@author: Ludvik Fjeld
+"""
+
+import math
+
+def spenning(R,I): return R*I #Volt
+
+def strøm(R,U): return U/R #Ampere
+
+def resistans(I,U): return U/I #Ohm
+
+def totRes(motstander):
+    motstander = sum([1/i for i in motstander])
+    return 1/motstander
+
+def effect(I,R,U=None):
+    if U == None:
+        return (I**2)*R
+    else:
+        return U*I
